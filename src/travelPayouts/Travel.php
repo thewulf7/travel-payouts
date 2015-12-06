a7dbd59136722146afdc324430fd3d33579de274
Johnny Utkin <thewulf7@me.com>
2015-12-06 12:18:58 +0300

    token setter added

--- src/travelPayouts/Travel.php
+++ src/travelPayouts/Travel.php
@@ -17,57 +17,24 @@ class Travel
     private $_client;
 
     /**
+     * @var string
+     */
+    private $_token;
+
+    /**
      * @param string $token
-     *
-     * @throws \RuntimeException
      */
-    public function __construct($token)
+    public function __construct($token = '')
     {
-        if ($token === '')
+        if ($token !== '')
         {
-            throw new \RuntimeException('Invalid token');
+            $this->setToken($token);
         }
-
-        $this->_client = new Client($token);
     }
 
-    /**
-     * Get tickets service
-     *
-     * @return services\Tickets
-     */
-    public function getTicketsService()
+    private function init()
     {
-        $service = new \travelPayouts\services\Tickets();
-        $service->setClient($this->getClient());
-
-        return $service;
-    }
-
-    /**
-     * Get data service
-     *
-     * @return services\Data
-     */
-    public function getDataService()
-    {
-        $service = new \travelPayouts\services\Data();
-        $service->setClient($this->getClient());
-
-        return $service;
-    }
-
-    /**
-     * Get flight service
-     *
-     * @return services\Flight
-     */
-    public function getFlightService()
-    {
-        $service = new \travelPayouts\services\Flight();
-        $service->setClient($this->getClient());
-
-        return $service;
+        $this->_client = new Client($this->getToken());
     }
 
     /**
@@ -78,5 +45,87 @@ class Travel
     public function getClient()
     {
         return $this->_client;
+    }
+
+    /**
+     * Get Token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->_token;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     *
+     * @return Travel
+     */
+    public function setToken($token)
+    {
+        $this->_token = $token;
+        $this->init();
+
+        return $this;
+    }
+
+    /**
+     * Get tickets service
+     *
+     * @return services\Tickets
+     * @throws \RuntimeException
+     */
+    public function getTicketsService()
+    {
+        if (!($this->getClient() instanceof Client))
+        {
+            throw new \RuntimeException('No token specified');
+        }
+
+        $service = new \travelPayouts\services\Tickets();
+        $service->setClient($this->getClient());
+
+        return $service;
+    }
+
+    /**
+     * Get data service
+     *
+     * @return services\Data
+     * @throws \RuntimeException
+     */
+    public function getDataService()
+    {
+        if (!($this->getClient() instanceof Client))
+        {
+            throw new \RuntimeException('No token specified');
+        }
+
+        $service = new \travelPayouts\services\Data();
+        $service->setClient($this->getClient());
+
+        return $service;
+    }
+
+    /**
+     * Get flight service
+     *
+     * @return services\Flight
+     * @throws \RuntimeException
+     */
+    public function getFlightService()
+    {
+        if (!($this->getClient() instanceof Client))
+        {
+            throw new \RuntimeException('No token specified');
+        }
+
+        $service = new \travelPayouts\services\Flight();
+        $service->setClient($this->getClient());
+
+        return $service;
     }
 }
